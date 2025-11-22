cadaec3f6bfb0a007ec407a9c352e4ccae07a419
ANAGH K SATHEESH <86482414+anaghksatheesh@users.noreply.github.com>
2025-11-22 11:22:18 +0530

    Add files via upload

---
+++ auth-parallax.js
@@ -0,0 +1,8 @@
+const bgText = document.getElementById("bgText");
+
+document.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 30;
+    const y = (e.clientY / window.innerHeight - 0.5) * 30;
+
+    bgText.style.transform = `translate(${x}px, ${y}px)`;
+});
